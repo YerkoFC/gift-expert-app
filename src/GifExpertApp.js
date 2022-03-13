e5733c3815367c63e960ea8a32d53506f00e9e21
Lucas Antonio <lucasa.carmonam@gmail.com>
2022-03-12 22:53:18 -0300

    feat: darkmode

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,51 +1,76 @@
-import React, { useState } from 'react'
-import AddCategory from './components/AddCategory'
-import GifGrid from './components/GifGrid';
+import React, { useState } from "react";
+import AddCategory from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
+import {
+  Player,
+  Controls,
+  LottieInteractive,
+} from "@lottiefiles/react-lottie-player";
+import { Button, Container, InputGroup } from "reactstrap";
+import { ThemeContext, themes } from "./theme/themeContext";
+import ToggleDark from "./components/ToggleDark";
 
-const GifExpertApp = ( props ) => {
-    const [categories, setCategories] = useState(['One Punch']);
-    // const handleCategories = () => {
-    //     let newCategory = 'Jujutsu Kaisen';
-    //     // Forma 1: Se usa cuando no se tiene acceso directo 
-    //     // a la variable del hook
-    //     setCategories( categories => [newCategory, ...categories]);
-    //     // Forma 2: Se usa cuando se tiene acceso a la variable ´newCategory´
-    //     setCategories([newCategory, ...categories]);
-    // }
- 
-    return (
-        <>
-            <h2> GifExpertApp </h2>
-            {/* 
+const GifExpertApp = (props) => {
+  const [darkMode, setDarkMode] = React.useState(true);
+  const [categories, setCategories] = useState(["One Punch"]);
+  // const handleCategories = () => {
+  //     let newCategory = 'Jujutsu Kaisen';
+  //     // Forma 1: Se usa cuando no se tiene acceso directo
+  //     // a la variable del hook
+  //     setCategories( categories => [newCategory, ...categories]);
+  //     // Forma 2: Se usa cuando se tiene acceso a la variable ´newCategory´
+  //     setCategories([newCategory, ...categories]);
+  // }
+
+  return (
+    <>
+      <div>
+        <h2> GifExpertApp </h2>
+        <InputGroup>
+          <ThemeContext.Consumer>
+            {({ changeTheme }) => (
+              <Button
+                onClick={() => {
+                  setDarkMode(!darkMode);
+                  changeTheme(darkMode ? themes.light : themes.dark);
+                }}
+              >
+                <Player
+                  autoplay
+                  loop
+                  src="https://assets7.lottiefiles.com/packages/lf20_mg5yaybb.json"
+                  style={{
+                    height: "150px",
+                    width: "150px",
+                    position: "relative",
+                  }}
+                ></Player>
+              </Button>
+            )}
+          </ThemeContext.Consumer>
+        </InputGroup>
+      </div>
+      {/* 
                 Se está enviando una funcion como prop al componente 
                 `AddCategory` 
             */}
-            <AddCategory  setCategories={ setCategories }/>
-            <hr/>
-            {/* <button onClick={handleCategories}> Agregar Categoria </button> */}
-            <ol>
-                { 
-                    categories.map( category => {
-                        // Generalmente, cuando se trabajan con datos desde
-                        // bases de datos, la key del elemento o dato a 
-                        // renderizar corresponde al id del dato, sin embargo
-                        // en esta ocasión colocaremos la misma categoria como 
-                        // key por que el índice no puede ser
-                        // React hace esto para saber qué elemento tiene que 
-                        // renderizar, y así no tener que renderizar todo de nuevo
-                        return <GifGrid 
-                                key={ category }
-                                category={ category }
-                                />
-                    }) 
-                }
-            </ol>
-        </>
-
-    )
-}
-
+      <AddCategory setCategories={setCategories} />
+      <hr />
+      {/* <button onClick={handleCategories}> Agregar Categoria </button> */}
+      <ol>
+        {categories.map((category) => {
+          // Generalmente, cuando se trabajan con datos desde
+          // bases de datos, la key del elemento o dato a
+          // renderizar corresponde al id del dato, sin embargo
+          // en esta ocasión colocaremos la misma categoria como
+          // key por que el índice no puede ser
+          // React hace esto para saber qué elemento tiene que
+          // renderizar, y así no tener que renderizar todo de nuevo
+          return <GifGrid key={category} category={category} />;
+        })}
+      </ol>
+    </>
+  );
+};
 
 export default GifExpertApp;
-
-
--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,50 +1,48 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const AddCategory = ({ setCategories }) => {
-    
-    // Es buena practica indicar que el hook será de un tipo definido
-    // ejemplo: en el hook de abajo, se indicara que la variable inputValue
-    // sera de tipo string, definiendola como string vacio
-    const [inputValue, setInputValue] = useState('')
-    
-    // @param: Evento, es quien recibe  
-    const handleInputChange = ( e ) => {
-        setInputValue(e.target.value);
-    }
+  // Es buena practica indicar que el hook será de un tipo definido
+  // ejemplo: en el hook de abajo, se indicara que la variable inputValue
+  // sera de tipo string, definiendola como string vacio
+  const [inputValue, setInputValue] = useState("");
 
-    const handleForm = (e) => {
-        e.preventDefault();
-        // Si el input es superior a 2, agregará una categoria 
-        // a la lista en el componente padre
-        if( inputValue.trim().length > 2 ) {
-            // Se hace de esta forma, dado que en este componente no se puede
-            // acceder directamente a la variable categories del componente
-            // `GifExpertApp`, sin embargo esto se puede hacer de igual forma
-            // enviando categories como otra prop a este componente 
-            setCategories( categories => [inputValue, ...categories]);
-            setInputValue('');
-        }
-    }
+  // @param: Evento, es quien recibe
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    return (
-        
-        <form onSubmit={ handleForm }>
-            <input 
-                type="text"
-                placeholder="Escribe aquí..." 
-                value={ inputValue }
-                onChange={ handleInputChange }
-            />
-            <h1> {inputValue} </h1> 
-        </form>
-        
-        
-    )
-}
+  const handleForm = (e) => {
+    e.preventDefault();
+    // Si el input es superior a 2, agregará una categoria
+    // a la lista en el componente padre
+    if (inputValue.trim().length > 2) {
+      // Se hace de esta forma, dado que en este componente no se puede
+      // acceder directamente a la variable categories del componente
+      // `GifExpertApp`, sin embargo esto se puede hacer de igual forma
+      // enviando categories como otra prop a este componente
+      setCategories((categories) => [inputValue, ...categories]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleForm}>
+        <input
+          type="text"
+          placeholder="Escribe aquí..."
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <h1> {inputValue} </h1>
+      </form>
+    </>
+  );
+};
 
 AddCategory.propTypes = {
-    setCategories: PropTypes.func.isRequired
-}
+  setCategories: PropTypes.func.isRequired,
+};
 
-export default AddCategory
+export default AddCategory;
